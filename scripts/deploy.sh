3cf42f75bec012d19bbbe9ba2bb567154f5081a0
Khmels <ser.khmel@gmail.com>
2022-01-04 21:59:55 +0200

    video13. Spring Boot. Deploy to server. 100%

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mvn clean package
+
+echo  'Copy files...'
+
+scp -i ~/.ssh/id_rsa \
+    target/serving-web-content-0.0.1-SNAPSHOT.jar \
+    serhio@192.168.0.105:/home/serhio/.dev/
+
+echo  'Restart server...'
+
+ssh -i ~/.ssh/id_rsa serhio@192.168.0.105 << EOF
+pgrep java | xargs kill -9
+nohup java -jar serving-web-content-0.0.1-SNAPSHOT.jar > log.txt &
+EOF
+
+echo 'Bye'
